f800d2fb7bd761fd9955eebd69325f104af92b9d
wjin0828 <wjin0828@gmail.com>
2026-03-20 17:30:02 +0000

    Deploying to gh-pages from @ wjin0828/wjin0828.github.io@c56bfe8f36e50afe841cf0addcd135fd3067bab9 🚀

--- assets/js/giscus-setup.js
+++ assets/js/giscus-setup.js
@@ -18,7 +18,7 @@ function determineGiscusTheme() {
 
   let giscusAttributes = {
     src: "https://giscus.app/client.js",
-    "data-repo": "wjin0828/al-folio",
+    "data-repo": "wjin0828/wjin0828.github.io",
     "data-repo-id": "",
     "data-category": "Comments",
     "data-category-id": "",
